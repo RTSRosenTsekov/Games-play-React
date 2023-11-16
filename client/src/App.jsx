fb30209dcf6fe74bfe052041f67cb4f289ddee88
Rosen.Tsekov <rts1914@yahoo.com>
2023-11-16 18:31:32 +0200

    Add header component and style

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,11 @@
+import Header from "./components/header/Header";
+
 function App() {
   return (
-    <>
+    <div id="box">
       <h1>Heloo Game Play</h1>
-    </>
+      <Header/>
+    </div>
   );
 }
 
