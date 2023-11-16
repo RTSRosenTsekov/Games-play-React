e296284a7d15d7baba15e51b035b9caaf9378d80
Rosen.Tsekov <rts1914@yahoo.com>
2023-11-16 22:52:00 +0200

    Show game list

--- client/src/services/gameService.js
+++ client/src/services/gameService.js
@@ -1,15 +1,23 @@
-const baseUrl = "http://localhost:3030/jsonstore";
+import { request } from "../lib/recuest";
 
-export const create =async (gameData) => {
-  const response = await fetch(`${baseUrl}/games`, {
+const baseUrl = "http://localhost:3030/jsonstore/games";
+
+export const create = async (gameData) => {
+  const response = await fetch(baseUrl, {
     method: "POST",
     headers: {
       "content-type": "application/json",
     },
     body: JSON.stringify(gameData),
   });
-  
+
   const result = await response.json();
-  
+
   return result;
+};
+
+export const getAll = async () => {
+  const result = await request("GET", baseUrl);
+  
+  return Object.values(result);
 };
