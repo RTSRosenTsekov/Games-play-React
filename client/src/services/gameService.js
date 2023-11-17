fd1ec27888830f43837608d20a6378a41c262973
Rosen.Tsekov <rts1914@yahoo.com>
2023-11-17 10:19:58 +0200

    Modify requester

--- client/src/services/gameService.js
+++ client/src/services/gameService.js
@@ -1,23 +1,23 @@
-import { request } from "../lib/recuest";
+import * as  request from "../lib/recuest";
 
 const baseUrl = "http://localhost:3030/jsonstore/games";
 
 export const create = async (gameData) => {
-  const response = await fetch(baseUrl, {
-    method: "POST",
-    headers: {
-      "content-type": "application/json",
-    },
-    body: JSON.stringify(gameData),
-  });
+//   const response = await fetch(baseUrl, {
+//     method: "POST",
+//     headers: {
+//       "content-type": "application/json",
+//     },
+//     body: JSON.stringify(gameData),
+//   });
 
-  const result = await response.json();
-
+//   const result = await response.json();
+    const result = await request.post(baseUrl,gameData)
   return result;
 };
 
 export const getAll = async () => {
-  const result = await request("GET", baseUrl);
+  const result = await request.get( baseUrl);
   
   return Object.values(result);
 };
