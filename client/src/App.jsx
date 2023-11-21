233646874082d8f405c18062bc2cb60e1e7cdd91
Rosen.Tsekov <rts1914@yahoo.com>
2023-11-21 18:06:01 +0200

    Add auth service and login redirect

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,46 @@
+import { useState } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+
+import * as authService from "./services/authService";
+import AuthContext from "./contexts/authContext";
+import Path from "./paths";
+
 import GameCreate from "./components/game-create/GameCreate";
 import GameList from "./components/game-list/GameList";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
-import { Routes, Route } from "react-router-dom";
 import Login from "./components/login/Login";
 import Register from "./components/register/Register";
 import GameDetails from "./components/game-details/GameDetails";
-import { useState } from "react";
 function App() {
-    const [auth,setAuth]=useState({});
-  
-    const loginSubmitHandler =(values)=>{
-      console.log(values);
-    }
-    
-  return (
-    <div id="box">
-      
-      <Header />
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/games" element={<GameList/>}/>
-        <Route path="/games/create" element={<GameCreate/>}/>
-        <Route path="/login" element={<Login loginSubmitHandler={loginSubmitHandler}/>}/>
-        <Route path="/register" element={<Register/>}/>
-        <Route path="/games/:gameId" element={<GameDetails/>}/>
-        
-        
-      </Routes>
-    </div>
+  const navigate = useNavigate();
+  const [auth, setAuth] = useState({});
+
+  const loginSubmitHandler = async (values) => {
+    const result = await authService.login(values.email, values.password);
+
+    setAuth(result);
+    
+    navigate(Path.Home)
+    
+  };
+
+  return (
+    <AuthContext.Provider value={{ loginSubmitHandler }}>
+      <div id="box">
+        <Header />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/games" element={<GameList />} />
+          <Route path="/games/create" element={<GameCreate />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/games/:gameId" element={<GameDetails />} />
+        </Routes>
+      </div>
+    </AuthContext.Provider>
   );
 }
 
--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,5 +1,7 @@
 import {Link} from 'react-router-dom';
 import useForm from '../../hooks/useForm';
+import { useContext } from 'react';
+import AuthContext from '../../contexts/authContext';
 
 const LoginFormKyes ={
 Email:'email',
@@ -7,10 +9,8 @@ Password:'password',
 };
 
 
-export default function Login({
-    loginSubmitHandler,
-
-}) {
+export default function Login() {
+    const {loginSubmitHandler}=useContext(AuthContext);
     const {values , onChange , onSubmit}= useForm(loginSubmitHandler,{
       email:'',
       password:''
